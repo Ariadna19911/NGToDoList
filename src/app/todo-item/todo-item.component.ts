d4ceab9119759d68f4a4bdd74048c4025f8d07d6
Ariadna Skoczek <askoczek@sii.pl>
2022-01-22 16:11:35 +0100

    typlista

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-  @Input() item;
+  @Input() item: TodoItem;
 
   constructor() { }
 
